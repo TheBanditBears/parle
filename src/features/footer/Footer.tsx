dd8c53b42b707fbb607f1a3a8e0390b1014bfc06
sarahchoww <chow.sarah@hotmail.com>
2022-09-17 03:02:01 -0400

    first commit, not done

---
+++ src/features/footer/Footer.tsx
@@ -0,0 +1,18 @@
+
+export function Footer() {
+    return (
+    <div>
+        <span>
+          <a
+            className="Github-link"
+            href="https://github.com/orgs/TheBanditBears/repositories"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src= "src/assets/images/github.png"/>
+
+          </a>
+        </span>
+    </div>
+    )
+  }
