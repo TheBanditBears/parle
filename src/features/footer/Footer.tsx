8d8849f402487e6faaabdcd3985a07a624dca872
sarahchoww <chow.sarah@hotmail.com>
2022-09-17 04:09:49 -0400

    footer

--- src/features/footer/Footer.tsx
+++ src/features/footer/Footer.tsx
@@ -1,11 +1,17 @@
-// import GitHubIcon from '@mui/icons-material/GitHub';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import CopyrightIcon from '@mui/icons-material/Copyright';
 
 export function Footer() {
     return (
-    <div>
-        <span>
+    <div className='footer'>
+
+        <p id='footer-top'>Made with love by Sarah and Niloy.</p>
+        <span >
+        <a id='black-text'>
+           <CopyrightIcon/> Copywright all rights reserved.
+        </a>
           <a
+            id='github black-text'
             href="https://github.com/orgs/TheBanditBears/repositories"
             target="_blank"
             rel="noopener noreferrer"
@@ -13,7 +19,7 @@ export function Footer() {
             <GitHubIcon/>
           </a>
         </span>
-        <GitHubIcon/>
+
     </div>
     )
   }
