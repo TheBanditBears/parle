f8555671acea5cd615d1abc32a122613a626d6ee
sarahchoww <chow.sarah@hotmail.com>
2022-09-18 07:30:26 -0400

    final changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
-import React from 'react';
 import './App.css';
 import Countup  from './features/countup/Countup'
 import {Sound} from './features/sound/Sound';
 import { Footer } from './features/footer/Footer';
-import { Score } from './features/Score/Score';
-import { UserScript } from './features/userScript/userScript';
 
 function App() {
   return (
     <div>
       <Countup />
       <Sound />
-      <Score />
-      <UserScript />
       <Footer/>
     </div>
   );
--- src/features/sound/Sound.tsx
+++ src/features/sound/Sound.tsx
@@ -1,14 +1,17 @@
 // Taken and inspired from https://medium.com/front-end-weekly/recording-audio-in-mp3-using-reactjs-under-5-minutes-5e960defaf10
 
 import { Button, ButtonGroup, Box } from "@mui/material";
+import parseBackend from "../../utils/axios-utils";
+import { useState } from "react";
 
 export function Sound() {
   const MicRecorder = require("mic-recorder-to-mp3");
+  const [parleData, setParleData] = useState('');
 
-  // New instance 
+  // New instance
   const recorder = new MicRecorder({
     bitRate: 128,
-  });   
+  });
   const startRecording = () => {
     // Start recording. Browser will request permission to use your microphone.
     recorder
@@ -40,32 +43,61 @@ export function Sound() {
 
         const player = new Audio(URL.createObjectURL(file));
         player.play();
+
+        mockDataReturn();
       })
       .catch((e: any) => {
         alert("We could not retrieve your message");
         console.log(e);
       });
+
+    const mockDataReturn = () => {
+      parseBackend
+        .post("/parle-score")
+        .then((response) => response.data)
+        .then(data => {
+            setParleData(JSON.stringify(data));
+        })
+        .then((data) => {
+            console.log(data)
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    };
   };
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        "& > *": {
-          m: 1,
-        },
-      }}
-    >
-      <ButtonGroup variant="text" aria-label="text button group">
-        <Button onClick={startRecording}>
-          Start Recording
-        </Button>
-        <Button onClick={stopRecording}>
-          Stop Recording
-        </Button>
-      </ButtonGroup>
-    </Box>
+    <div>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          "& > *": {
+            m: 1,
+          },
+        }}
+      >
+        <ButtonGroup variant="text" aria-label="text button group">
+          <Button onClick={startRecording}>Start Recording</Button>
+          <Button onClick={stopRecording}>Stop Recording</Button>
+        </ButtonGroup>
+      </Box>
+
+      <span style={{ textAlign: "center" }}>
+        <textarea rows={15} cols={60} name="description">
+          Enter presentation script here...
+        </textarea>
+      </span>
+
+      <span style={{ textAlign: "center" }}>
+        <textarea rows={15} cols={60} name="description" value={parleData ? parleData : ''}>
+        </textarea>
+      </span>
+
+      <h3>Presentation Score:</h3>
+      
+    </div>
   );
 }
